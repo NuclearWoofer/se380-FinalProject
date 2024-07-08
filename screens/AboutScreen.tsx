9a5e33eb683b117ff0c0a8bf0eed87b01b71c674
Michael Lopez <mlopez2@harriscomputer.com>
2024-07-08 13:04:25 -0400

    About Screen tweaks, gradle tweaks

--- screens/AboutScreen.tsx
+++ screens/AboutScreen.tsx
@@ -1,57 +1,59 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity, Linking } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity, Linking, ScrollView } from 'react-native';
 
 function AboutScreen() {
   return (
-    <View style={styles.container}>
-      <Image
-        source={require('../images/hs1.png')}
-        style={styles.image}
-      />
-      <Text style={styles.title}>About Michael</Text>
-      <Text style={styles.text}>
-        I present my ongoing personal project developed in React Native, initially conceived as part of the SE380 React Native course, now serving as a platform for refining my React skills.
-      </Text>
-      <Text>
-        Key highlights of this project include:
-      </Text>
-      <Text></Text>
-      <Text>
-        - Implementation of Typescript: Leveraging the robustness and type safety offered by Typescript to ensure a more reliable and maintainable codebase.
-      </Text>
-      <Text>
-        - Nested Navigation: Employing nested navigation structures to enhance user experience and streamline app navigation flow.
-      </Text>
-      <Text>
-        - Integration of Expo SDK: Utilizing the powerful capabilities of the Expo SDK, I've seamlessly integrated essential functionalities such as making HTTP requests and incorporating Expo SDK components to enrich the app's features.
-      </Text>
-      <Text>
-        - Dynamic Animation: Enhancing user engagement through dynamic animations, albeit a small amount, however, less is more!
-      </Text>
-      <Text style={styles.text}>
-        This application serves as a platform for me to indulge in one of my cherished hobbies: miniature painting. I love playing tabletop games such as Dungeons & Dragons, Warhammer, etc. These games can typically be played with miniature figures representing characters in those games. These miniatures are often purchased unpainted and unassembled. One of my favorite things to do is paint these miniatures!
-      </Text>
-      <View style={styles.linksContainer}>
-        <TouchableOpacity
-          style={styles.link}
-          onPress={() => Linking.openURL('https://www.linkedin.com/in/michael-lopez-a7436b157/')}
-        >
-          <Text style={styles.linkText}>LinkedIn</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.link}
-          onPress={() => Linking.openURL('mailto:miketatooine@gmail.com')}
-        >
-          <Text style={styles.linkText}>Hire Me</Text>
+    <ScrollView contentContainerStyle={styles.scrollViewContainer}>
+      <View style={styles.container}>
+        <Image
+          source={require('../images/hs1.png')}
+          style={styles.image}
+        />
+        <Text style={styles.title}>About Michael</Text>
+        <Text style={styles.text}>
+          This application serves as a platform for me to indulge in one of my cherished hobbies: miniature painting. I love playing tabletop games such as Dungeons & Dragons, Warhammer, etc. These games can typically be played with miniature figures representing characters in those games. These miniatures are often purchased unpainted and unassembled. One of my favorite things to do is paint these miniatures!
+        </Text>
+        <Text>
+          Key highlights of this project include:
+        </Text>
+        <Text></Text>
+        <Text>
+          - Implementation of Typescript: Leveraging the robustness and type safety offered by Typescript to ensure a more reliable and maintainable codebase.
+        </Text>
+        <Text>
+          - Nested Navigation: Employing nested navigation structures to enhance user experience and streamline app navigation flow.
+        </Text>
+        <Text>
+          - Integration of Expo SDK: Utilizing the powerful capabilities of the Expo SDK, I've seamlessly integrated essential functionalities such as making HTTP requests and incorporating Expo SDK components to enrich the app's features.
+        </Text>
+        <Text>
+          - Dynamic Animation: Enhancing user engagement through dynamic animations, albeit a small amount, however, less is more!
+        </Text>
 
-
-        </TouchableOpacity>
+        <View style={styles.linksContainer}>
+          <TouchableOpacity
+            style={styles.link}
+            onPress={() => Linking.openURL('https://www.linkedin.com/in/michael-lopez-a7436b157/')}
+          >
+            <Text style={styles.linkText}>LinkedIn</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.link}
+            onPress={() => Linking.openURL('mailto:miketatooine@gmail.com')}
+          >
+            <Text style={styles.linkText}>Hire Me</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  scrollViewContainer: {
+    flexGrow: 1,
+    justifyContent: 'center',
+  },
   container: {
     flex: 1,
     alignItems: 'center',
@@ -64,7 +66,6 @@ const styles = StyleSheet.create({
     marginTop: 20,
     borderRadius: 10,
   },
-
   title: {
     fontSize: 24,
     fontWeight: 'bold',
